e292f33b2387587098ffba9fc7606a8b9ba10e53
asher <asher01000001@gmail.com>
2023-09-02 09:18:00 +0800

    add golang helloworld

---
+++ Day1/hw/go.mod
@@ -0,0 +1,3 @@
+module asherl
+
+go 1.21.0
---
+++ Day1/hw/hw.go
@@ -0,0 +1,9 @@
+// All files in the same folder must have the same package clause
+package main
+
+// Package for input and output
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Asher :) ")
+}
